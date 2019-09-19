00af0355157e97cd70ab1865288af866dc7d454b
Tim Eckle <tim.eckle@stud-mail.uni-wuerzburg.de>
2019-09-19 18:19:26 +0200

    Added SqueezeNet as pretrained net for the RGB-predictor.

---
+++ squeezenet_util.py
@@ -0,0 +1,62 @@
+import torchvision.models as models
+from torchvision import transforms
+import torch
+import torch.nn as nn
+from PIL import Image
+
+squeezynet = models.squeezenet1_1(pretrained=True)
+
+# this vs with torch.no_grad()
+#squeezynet.eval()
+
+# change layers https://pytorch.org/docs/master/notes/autograd.html
+for param in squeezynet.parameters():
+    param.requires_grad = False
+
+def main():
+    # Replace the fully-connected last part https://pytorch.org/docs/stable/_modules/torchvision/models/squeezenet.html
+    final_conv = nn.Conv2d(512, 3, kernel_size=1)
+    squeezynet.classifier = nn.Sequential(
+                nn.Dropout(p=0.5),
+                final_conv,
+                nn.ReLU(inplace=True),
+                nn.AdaptiveAvgPool2d((1, 1))
+            )
+
+    # sample execution https://pytorch.org/hub/pytorch_vision_squeezenet/
+    input_image = Image.open('image_path')
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+    input_tensor = preprocess(input_image)
+    input_batch = input_tensor.unsqueeze(0)
+
+    with torch.no_grad():
+        output = squeezynet(input_batch)
+
+    print(output[0])
+    print(torch.nn.functional.softmax(output[0], dim=0))
+
+def run_and_get_values(final_conv, images):
+    output = []
+
+    # TODO: is this following line possible or do I have to init a squeeze net
+    squeezynet.classifier = nn.Sequential(
+                nn.Dropout(p=0.5),
+                final_conv,
+                nn.ReLU(inplace=True),
+                nn.AdaptiveAvgPool2d((1, 1))
+            )
+
+    with torch.no_grad():
+        for image_vector in images:
+            output_value = squeezynet(image_vector)
+            output.append(torch.nn.functional.softmax(output_value[0], dim=0))
+
+    return output
+
+if __name__ == '__main__':
+    main()
